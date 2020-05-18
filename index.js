5b6ac9a52947e81350563a6c0e3d89c0b30b1bb8
Sahil Bansal <56014553+Sahil-Bansal-prog@users.noreply.github.com>
2020-05-18 21:51:22 +0530

    This is done by me

---
+++ index.js
@@ -0,0 +1,26 @@
+//1. ways to write in javascript 
+// alert("This is not me");
+// document.write("This is written on document")
+//2. javascript Console api
+// console.log("This is log", 7 + 188, "This is also waste")
+// console.warn("This is a warning")
+// console.error("THis is an error")
+
+// 3. Variables
+var number1 = 76;
+var number2 = 34;
+console.log(number1+number2);
+
+//4. Data types in js   
+var num1 = 343;
+var num2 = 80.67;
+
+var str1 = "Ramesh";
+var str2 = " is a rokkie";
+
+var marks = {
+        ravi: 92.23,
+        raku: 74,
+        harry: 93
+}
+
